884b696a433b6a05d32bfe953a2fdee04f8745e7
aliilter <aliilter@hotmail.com>
2026-01-18 05:38:41 -0600

    console.log(double(5));

---
+++ PREDICTIONS.md
@@ -0,0 +1,3 @@
+## Prediction 1 - double(n)
+- double(5) should return 10
+- double(-3) should return -6
---
+++ math.js
@@ -0,0 +1,9 @@
+// math.js - CS81 Module 3B Toolkit
+
+function double(n) {
+    return n * 2;
+}
+
+
+//test output
+console.log(double(5));
